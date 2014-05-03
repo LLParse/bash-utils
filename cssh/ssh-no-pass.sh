bc78cfdd0d83f881723c4f596075944ee240e149
James D. Oliver <james.oliver@pegs.com>
2014-05-03 03:12:33 -0700

    Add script for SSH keypair generation / public key authorization

---
+++ cssh/ssh-no-pass.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -Eu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: James Oliver
+# Date:   2014-05-03
+#
+trap error 1 2 3 15 ERR
+
+error() {
+  echo "SSH No Password Exited Abnormally"
+  exit 1
+}
+
+trust-key() {
+  for host in "$@"; do
+    echo "Configuring password-less SSH on $host.."
+    scp ~/.ssh/id_rsa.pub $host:~
+    ssh $host 'cat ~/id_rsa.pub >> ~/.ssh/authorized_keys2 && rm ~/id_rsa.pub'
+  done
+}
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+  echo "Creating KeyPair on `hostname`.."
+  ssh-keygen
+fi
+trust-key $@
